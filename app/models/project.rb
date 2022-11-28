47f90c4fa26f6376dbe4adf549d1be7322cd43fa
Dimitri Depardieu <dimitridepardieu@gmail.com>
2022-11-28 18:00:38 +0100

    Create DB and setup active storage for cloudinary

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,8 @@ class Project < ApplicationRecord
   has_many :likes
   belongs_to :user
 
+  has_one_attached :photo
+
   validates :title, presence: true
   validates :description, presence: true
   validates :category, presence: true, inclusion: { in: CATEGORIES }
