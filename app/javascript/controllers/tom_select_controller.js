9485cb499108a0d014b284ef4fbc8969399cec19
Dimitri Depardieu <dimitridepardieu@gmail.com>
2022-12-05 15:52:31 +0100

    Connect tom-select data-controller

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
+import TomSelect from "tom-select";
 
 // Connects to data-controller="tom-select"
 export default class extends Controller {
+  static targets = ["test"]
+
   connect() {
+    console.log("TEST2")
+    console.log(this.testTarget)
+
+    new TomSelect(this.element)
+    console.log("Hello from Tom Select!")
   }
 }
