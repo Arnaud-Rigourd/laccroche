ef0adcfea04aa7c12ce69362c7fe78ee44c183df
Dimitri Depardieu <dimitridepardieu@gmail.com>
2022-12-05 12:21:32 +0100

    Install select2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,10 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
 import simpleParallax from 'simple-parallax-js'
+
+// Select2
+import { initSelect2 } from '../components/init_select2';
+
+document.addEventListener("turbolinks:load", function() {
+  initSelect2();
+});
