6689369029f87a747f20e88881a848809897b06b
Dimitri Depardieu <dimitridepardieu@gmail.com>
2022-12-06 12:37:42 +0100

    Implement action cable in chatrooms

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,12 @@ class MessagesController < ApplicationController
     @message.user = current_user
 
     if @message.save
-      redirect_to chatroom_path(@chatroom)
+      # redirect_to chatroom_path(@chatroom)
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "message", locals: { message: @message })
+      )
+      head :ok
     else
       render "chatrooms/show", status: :unprocessable_entity
     end
