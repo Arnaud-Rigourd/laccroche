df443b027518e4a59efa0e6262e68ed86ba2c0ad
Dimitri Depardieu <dimitridepardieu@gmail.com>
2022-12-08 11:15:22 +0100

    Add pseudo-code to render chatrooms in the chatbox

--- app/javascript/controllers/chatrooms_controller.js
+++ app/javascript/controllers/chatrooms_controller.js
@@ -2,15 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="chatrooms"
 export default class extends Controller {
+  // TODO: 2 nouvelles targets : listing des chatrooms + chatroom
   static targets = ["chatbox"]
 
   connect() {
     console.log("Hello from chatrooms stimulus controller!")
   }
 
-  open(event) {
+  openChatbox(event) {
     event.preventDefault()
-    console.log("Method open is connected!")
+    console.log("Method openChatbox is connected!")
     this.chatboxTarget.classList.toggle("d-none")
   }
+
+  openChatroom(event) {
+    event.preventDefault()
+    console.log("Method openChatroom is connected!")
+
+    // TODO: jouer avec la target de listing des chatrooms, et les targetSSSSS chatroom
+    // acces event.target.dataset.chatroomID => ex 2
+
+    // jouer avec la classe d-none a ajouter ou retirer sur les targets
+  }
+
+
 }
